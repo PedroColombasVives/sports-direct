f360764780fc0344398f011cb54a116a59046e05
lucy-goodchild <Lucy.goodchild@publicissapient.com>
2022-10-13 15:02:33 +0100

    sportcards created

--- sports-direct/src/SportPage/components/SportCardItem.js
+++ sports-direct/src/SportPage/components/SportCardItem.js
@@ -1,1 +1,27 @@
 //DESCRIBES WHAT EACH INDIVIDUAL ITEM WILL SHOW - IMAGE, SPORT NAME, ETC.
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+export default function SportCardItem(props) {
+  return (
+    <Card sx={{ width: 420}}>
+      <CardActionArea href="">
+        <CardMedia
+          component="img"
+          height="350"
+          image={props.image}
+        />
+        <CardContent>
+        <Typography level="h2" fontSize="lg" textColor="#fff" mb={1}>
+          {props.sport}
+        </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
--- sports-direct/src/SportPage/components/SportCardList.js
+++ sports-direct/src/SportPage/components/SportCardList.js
@@ -1,1 +1,23 @@
 // DESCRIBES HOW THET LIST WILL STRUCTURE EACH OF THE CARD ITEMS.
+import React from "react";
+import { Stack } from "@mui/system";
+import SportCardItem from "./SportCardItem";
+
+export default function SportCardList(props) {
+    return (
+        <div>
+        <Stack mt={5} ml={5} mr={5} mb={5} direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={8}>
+            <SportCardItem sport="Football"></SportCardItem>
+            <SportCardItem sport="Football"></SportCardItem>
+      </Stack>
+      <Stack mt={5} ml={5} mr={5} mb={5} direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={8}>
+            <SportCardItem sport="Football"></SportCardItem>
+            <SportCardItem sport="Football"></SportCardItem>
+      </Stack>
+      <Stack mt={5} ml={5} mr={5} mb={5} direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={8}>
+            <SportCardItem sport="Football"></SportCardItem>
+            <SportCardItem sport="Football"></SportCardItem>
+      </Stack>
+      </div>
+    );
+  }
